4386dc2ea13ccca643362349ea18c8a19ad44b94
Constance Schmidt <coco390@gmail.com>
2024-08-11 14:45:17 -0400

    JS updated

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
       button.querySelector('img').style.filter = 'brightness(0) invert(1)';
       button.style.backgroundColor = '#6E8098';
     });
+
+    button.addEventListener('click', () => {
+        const social = document.querySelector('#social-window');
+        social.classList.toggle('show');
+    })
+
 //Resetting the state
     button.addEventListener('mouseout', () => {
       button.querySelector('img').style.filter = 'invert(0%)';
@@ -15,4 +21,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
-//Adding mouse click event listener
